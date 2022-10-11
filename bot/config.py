0d908746734692a3d791896266a45e71f36b3cc4
Samir <64221555+SamirJanaOfficial@users.noreply.github.com>
2022-10-11 23:37:46 +0530

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,27 @@
+#    This file is part of the Compressor distribution.
+#    Copyright (c) 2021 Danish_00
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
+
+from decouple import config
+
+try:
+    APP_ID = config("APP_ID", cast=int)
+    API_HASH = config("API_HASH")
+    BOT_TOKEN = config("BOT_TOKEN")
+    DEV = 568768212
+    OWNER = config("OWNER")
+except Exception as e:
+    LOGS.info("something went wrong")
+    LOGS.info(str(e))
+    exit(1)
